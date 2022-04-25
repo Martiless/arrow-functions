da0d310e71500a8bdec7bd98e62ab570a600c3cf
Martina Martin <marti.law@gmail.com>
2022-04-25 09:31:30 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a,b) {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum)
 
 // Arrow Function With Parameters
+const addTwoNumbers2 = (a,b) => {
+    return a + b;
+}
+let answer = addTwoNumbers2(3, 8);
+console.log(answer)
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a, b) => a + b;
+let sum2 = addTwoNumbers3(6, 4);
+console.log(sum2)
 
 // Implicit Returns
+const saySomethings = message => console.log(message);
+saySomethings('Hello There!!')
+
+const sayHello = () => console.log('hello')
+sayHello()
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiline string!
+    <p>`
+)
+console.log(returnMultipleLines());
